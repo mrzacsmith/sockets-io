a6f6978f9d62625203a2a80ff4811f90fae9c3f6
Zac Smith <zrs3141592@gmail.com>
2021-06-15 14:32:21 -0600

    basic socket server/client working

--- socket101/chat.js
+++ socket101/chat.js
@@ -1,6 +1,17 @@
 const express = require('express')
+const socketio = require('socket.io')
 const app = express()
 
 app.use(express.static(__dirname + '/public'))
 
-app.listen(9200, () => console.log('chat server is running'))
+const expressServer = app.listen(9200, () =>
+  console.log('chat server is running')
+)
+
+const io = socketio(expressServer)
+io.on('connection', (socket) => {
+  socket.emit('messageFromServer', { data: 'This is from the server' })
+  socket.on('messageToServer', (dataFromClient) => {
+    console.log(dataFromClient)
+  })
+})
